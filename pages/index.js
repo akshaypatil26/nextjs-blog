598ae0b4767baed20d9a0d405822c62cb7ae422d
Akshay Patil <akshaypatil26@hotmail.com>
2023-02-25 21:15:54 +0530

    added display picture on home page for testing

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,10 @@ import Link from "next/link";
 import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
 import Date from "../components/date";
-
+import Image from "next/image";
 import { getSortedPostsData } from "../lib/posts";
+
+const name = "Akshay S. Patil";
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
@@ -21,6 +23,17 @@ export default function Home({ allPostsData }) {
       <Head>
         <title>{siteTitle}</title>
       </Head>
+      <div className="displaypicture">
+        <Image
+          priority
+          src="/images/profile.jpg"
+          className={utilStyles.borderCircle}
+          height={144}
+          width={144}
+          alt=""
+        />
+        <h1 className={utilStyles.heading2Xl}>{name}</h1>
+      </div>
       <section className={utilStyles.headingMd}>
         <p>
           I'm <b>Akshay Patil</b>. I am a UX designer and Front-end engineer.
@@ -29,7 +42,6 @@ export default function Home({ allPostsData }) {
           from Coursera and diving deep into React JS.{" "}
         </p>
       </section>
-
       {/* Add this <section> tag below the existing <section> tag */}
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>

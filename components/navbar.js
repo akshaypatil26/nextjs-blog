23e977ffa508131ef2b023527e6f9c504cc47ed6
Akshay Patil <akshaypatil26@hotmail.com>
2023-03-21 21:17:35 +0530

    added js map in navbar design for links

--- components/navbar.js
+++ components/navbar.js
@@ -2,17 +2,31 @@ import styles from "./navbar.module.css";
 export default function Navbar() {
   return (
     <>
-      <div className={styles.navbar}>
+      <nav className={styles.navbar}>
+        {[
+          ["Home", "/"],
+          ["Logs", "/logs"],
+          // ["UX designs", "/uxdesigns"],
+          // ["Experience", "/experience"],
+        ].map(([title, url]) => (
+          <a
+            href={url}
+            className="px-3 hover:text-slate-400"
+          >
+            {title}
+          </a>
+        ))}
+      </nav>
+
+      {/* <div className={styles.navbar}>
         <li>
           <a href="/">Home</a>
         </li>
         <li>
           <a href="/logs">Logs</a>
         </li>
-        {/* <li>
-          <a href="/socials">Socials</a>
-        </li> */}
-      </div>
+       
+      </div> */}
     </>
   );
 }
